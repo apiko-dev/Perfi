9286dfec15c31a77a23f7a7a38d5efb8f3bb7aec
danilichev <v.danilichev@gmail.com>
2017-04-08 15:16:05 +0300

    Added redux

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
+// import { addNavigationHelpers } from 'react-navigation';
 import Navigator from './app/navigation';
 import styles from './app/styles/AppStyles';
 
