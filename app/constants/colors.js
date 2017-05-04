494e4ce527fb5c6428210d5caa90296cbbbce689
danilichev <v.danilichev@gmail.com>
2017-05-04 16:34:24 +0300

    Added default colors

---
+++ app/constants/colors.js
@@ -0,0 +1,24 @@
+export default {
+  // material teal/amber schema
+  darkPrimary: '#00796B',
+  defaultPrimary: '#009688',
+  lightPrimary: '#B2DFDB',
+  accent: '#FFC107',
+  primaryText: '#212121',
+  secondaryText: '#757575',
+  divider: '#BDBDBD',
+
+  // status colors
+  info: '#0288D1',
+  success: '#4CAF50',
+  warning: '#FFC107',
+  error: '#F44336',
+
+  // additional
+  white: '#ffffff',
+  black: '#000000',
+  gray: '#848b90',
+  lightGray: '#F5F5F5',
+  lightShadow: 'rgba(0, 0, 0, 0.05)',
+  darkShadow: 'rgba(0, 0, 0, 0.4)',
+};
