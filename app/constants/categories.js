0650b0518663744f5668ce3783226fe36ddc5b63
danilichev <v.danilichev@gmail.com>
2017-05-08 16:26:10 +0300

    Little changes

---
+++ app/constants/categories.js
@@ -0,0 +1,9 @@
+export const categoriesTypes = {
+  expense: 'expense',
+  income: 'income',
+};
+
+export const defaultCategories = [
+  { name: 'Salary', icon: 'cash', type: categoriesTypes.income },
+  { name: 'Bills', icon: 'tag', type: categoriesTypes.expense },
+];
