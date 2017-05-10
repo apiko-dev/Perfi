e31f526edf89059f8b95bccfad060d121205d7ce
danilichev <v.danilichev@gmail.com>
2017-05-10 14:28:35 +0300

    Updated styles, added action button to screen

--- app/screens/Categories.js
+++ app/screens/Categories.js
@@ -1,17 +1,23 @@
 import React, { PropTypes } from 'react';
 import { Alert, Platform, View } from 'react-native';
 import screens from '../constants/screens';
-import { DrawerButton } from '../components';
+import { DrawerButton, RoundButton } from '../components';
 import { CategoriesListContainer } from '../containers';
+import styles from '../styles/CategoriesStyles';
 
 const Categories = ({ navigation }) => {
   const currentRoute = navigation.state.routeName;
 
   return (
-    <View>
+    <View style={styles.rootStyle}>
       <CategoriesListContainer
         type={currentRoute.toLowerCase()}
         onSelectCategory={id => Alert.alert(id)}
+      />
+      <RoundButton
+        style={styles.addButtonStyle}
+        iconName="add"
+        onPress={() => navigation.navigate(screens.CategoryEditor, { title: 'Add category' })}
       />
     </View>
   );
