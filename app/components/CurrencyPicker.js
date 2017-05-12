ea0bce479cd470918b49bf15314b35b2f79cb530
AndreyKogut <andreyko.ol@gmail.com>
2017-05-12 13:22:31 +0300

    Currency picker refactored

--- app/components/CurrencyPicker.js
+++ app/components/CurrencyPicker.js
@@ -8,33 +8,29 @@ import {
 import currencies from '../constants/currencies';
 import styles from '../styles/FormStyles';
 
-const CurrencyPicker = ({ onValueChange, selectedValue = currencies[0] }) => {
-  const getLabel = ({ name, sign }) => `${name}(${sign})`;
+const getLabel = ({ name, sign }) => `${name}(${sign})`;
 
-  const getListItem = value => (
-    <MenuOption
-      key={getLabel(value)}
-      value={value}
-      text={getLabel(value)}
+const getListItem = value => (
+  <MenuOption
+    key={getLabel(value)}
+    value={value}
+    text={getLabel(value)}
+  />
+);
+
+const CurrencyPicker = ({ onValueChange, selectedValue = currencies[0] }) => (
+  <Menu
+    onSelect={onValueChange}
+    style={styles.dropDownMenuStyle}
+  >
+    <MenuTrigger
+      text={getLabel(selectedValue)}
     />
-  );
-
-  const getCurrenciesListItems = () => currencies.map(getListItem);
-
-  return (
-    <Menu
-      onSelect={onValueChange}
-      style={styles.dropDownMenuStyle}
-    >
-      <MenuTrigger
-        text={getLabel(selectedValue)}
-      />
-      <MenuOptions>
-        { getCurrenciesListItems() }
-      </MenuOptions>
-    </Menu>
-  );
-};
+    <MenuOptions>
+      { currencies.map(getListItem) }
+    </MenuOptions>
+  </Menu>
+);
 
 CurrencyPicker.propTypes = {
   onValueChange: PropTypes.func,
