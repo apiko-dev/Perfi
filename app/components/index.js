7746de7c198d78df510d237f93c35eb10fa7039e
Andiy Kohut <andreyko.ol@gmail.com>
2017-05-15 16:38:33 +0300

    Accounts (#16)
    * Accounts actions/reducers, line separators changed to LF
    
    * Icons picker, defaults for adding/editing accounts
    
    * SceneContentWrapper, TextFieldWithIcon, SmallButton and Input field additional components
    
    * Date picker for android, select picker, editing/creating form for accounts
    
    * Create account
    
    * Accounts list, editing
    
    * Show/hide modal update, account deleting
    
    * Accounts merged with master and refactored
    
    * Merge accounts with master (#2)
    
    * Accounts and categories tests(fix)
    
    * Transactions reducer tests(fix)
    
    * Transfers tests(fix)
    
    * Pull request fixes(without date picker and modal hiding), account form enhancer refactored
    
    * Icons picker hiding, Currency picker and Date picker reworked, components refactored, forms styles update
    
    * Merged with master(#4). Picker structure refactored, anonymous functions form .map removed
    
    * Icons picker refactored according to app style
    
    * Icons picker refactored according to app style(#2)
    
    * Default props fix
    
    * Currency picker refactored
    
    * Migration to navigator v1.0.0-beta.9
    
    * Vector icons package deleted, components index file refactored
    
    * Navigation icons fix, components index refactored
    
    * InputField and InputFieldWith icons component removed, AccountsForm screen is getting function for submit now

--- app/components/index.js
+++ app/components/index.js
@@ -5,17 +5,14 @@ import CategoriesList from './categoriesList';
 import DeleteButton from './navButtons/DeleteButton';
 import CurrencyPicker from './CurrencyPicker';
 import DrawerButton from './DrawerButton';
-import FixedButtonsContainer from './FixedButtonsContainer';
 import FormInputWithIcon from './FormInputWithIcon';
 import IconsPickerModal from './IconsPicker';
-import InputField from './InputField';
 import Loading from './Loading';
 import NavIcon from './NavIcon';
 import PropsProxyHOC from './PropsProxyHOC';
 import RoundButton from './RoundButton';
 import SceneContentWrapper from './SceneContentWrapper';
 import TransactionForm from './transactionForm';
-import TextFieldWithIcon from './TextFieldWithIcon';
 import TouchableFormInput from './TouchableFormInput';
 
 export {
@@ -26,16 +23,13 @@ export {
   CurrencyPicker,
   DrawerButton,
   DeleteButton,
-  FixedButtonsContainer,
   FormInputWithIcon,
   IconsPickerModal,
-  InputField,
   Loading,
   NavIcon,
   PropsProxyHOC,
   RoundButton,
   SceneContentWrapper,
   TransactionForm,
-  TextFieldWithIcon,
   TouchableFormInput,
 };
