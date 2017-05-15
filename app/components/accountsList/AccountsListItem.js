b448b869ef1d3dea9999b73408d50ef85fa51cde
AndreyKogut <andreyko.ol@gmail.com>
2017-05-15 18:57:09 +0300

    Calculator added to account form

--- app/components/accountsList/AccountsListItem.js
+++ app/components/accountsList/AccountsListItem.js
@@ -1,12 +1,21 @@
 import React, { PropTypes } from 'react';
+import { Text, View } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import styles from '../../styles/CategoriesListItemStyles';
+import accountsListStyle from '../../styles/FormStyles';
 
-const AccountsListItem = ({ name, icon, style, ...props }) => (
+const { listItemStyle, textStyle } = accountsListStyle;
+
+const AccountsListItem = ({ name, icon, initialBalance, style, ...props }) => (
   <ListItem
     containerStyle={[styles.rootStyle, style]}
     {...props}
-    title={name}
+    title={
+      <View style={listItemStyle}>
+        <Text style={textStyle}>{ name }</Text>
+        <Text>{ initialBalance }</Text>
+      </View>
+    }
     leftIcon={{
       name: icon,
       type: 'material-community',
@@ -19,6 +28,10 @@ AccountsListItem.propTypes = {
   style: PropTypes.any,
   name: PropTypes.string,
   icon: PropTypes.string,
+  initialBalance: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]),
 };
 
 export default AccountsListItem;
