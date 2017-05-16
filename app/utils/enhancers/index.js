2e83346210584e5f6d1da7f6454c5c78f6051dbd
danilichev <v.danilichev@gmail.com>
2017-05-16 15:19:25 +0300

    Added Collapse element

--- app/utils/enhancers/index.js
+++ app/utils/enhancers/index.js
@@ -1,7 +1,9 @@
 import withNonOptimalStates from './withNonOptimalStates';
 import withStyle from './withStyle';
+import withToggle from './withToggle';
 
 export {
   withNonOptimalStates,
   withStyle,
+  withToggle,
 };
---
+++ app/utils/testTransactions.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    value: 10,
+    account: 0,
+    category: 1,
+    note: 'test note',
+  }, {
+    value: 20,
+    account: 0,
+    category: 1,
+    note: 'test note',
+  }, {
+    value: 30,
+    account: 0,
+    category: 1,
+    note: 'test note',
+  },
+  {
+    value: 10,
+    account: 0,
+    category: 1,
+    note: 'test note',
+  }, {
+    value: 20,
+    account: 0,
+    category: 1,
+    note: 'test note',
+  }, {
+    value: 30,
+    account: 0,
+    category: 1,
+    note: 'test note',
+  },
+];
