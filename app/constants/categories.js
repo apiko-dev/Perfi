ea446d62128b8300079c90f82df861ae86ec7428
danilichev <v.danilichev@gmail.com>
2017-05-23 17:38:23 +0300

    Refactored default categories

--- app/constants/categories.js
+++ app/constants/categories.js
@@ -1,22 +1,35 @@
+import R from 'ramda';
+
 export const categoriesTypes = {
   expense: 'expense',
   income: 'income',
 };
 
+const incomeCategories = [
+  { name: 'Salary', icon: 'cash' },
+  { name: 'Savings', icon: 'coin' },
+  { name: 'Deposits', icon: 'castle' },
+];
+
+const expenseCategories = [
+  { name: 'Bills', icon: 'tag' },
+  { name: 'Car', icon: 'car' },
+  { name: 'Communications', icon: 'phone' },
+  { name: 'Eating Out', icon: 'silverware' },
+  { name: 'Entertainment', icon: 'martini' },
+  { name: 'Food', icon: 'food' },
+  { name: 'Gifts', icon: 'gift' },
+  { name: 'Health', icon: 'heart-pulse' },
+  { name: 'Home', icon: 'home-variant' },
+  { name: 'Pets', icon: 'cat' },
+  { name: 'Sport', icon: 'dumbbell' },
+  { name: 'Taxi', icon: 'taxi' },
+];
+
+const withType = type => category => ({ ...category, type });
+const allWithType = type => R.map(withType(type));
+
 export const defaultCategories = [
-  { name: 'Salary', icon: 'cash', type: categoriesTypes.income },
-  { name: 'Savings', icon: 'coin', type: categoriesTypes.income },
-  { name: 'Deposits', icon: 'castle', type: categoriesTypes.income },
-  { name: 'Bills', icon: 'tag', type: categoriesTypes.expense },
-  { name: 'Car', icon: 'car', type: categoriesTypes.expense },
-  { name: 'Communications', icon: 'phone', type: categoriesTypes.expense },
-  { name: 'Eating Out', icon: 'silverware', type: categoriesTypes.expense },
-  { name: 'Entertainment', icon: 'martini', type: categoriesTypes.expense },
-  { name: 'Food', icon: 'food', type: categoriesTypes.expense },
-  { name: 'Gifts', icon: 'gift', type: categoriesTypes.expense },
-  { name: 'Health', icon: 'heart-pulse', type: categoriesTypes.expense },
-  { name: 'Home', icon: 'home-variant', type: categoriesTypes.expense },
-  { name: 'Pets', icon: 'cat', type: categoriesTypes.expense },
-  { name: 'Sport', icon: 'dumbbell', type: categoriesTypes.expense },
-  { name: 'Taxi', icon: 'taxi', type: categoriesTypes.expense },
+  ...allWithType(categoriesTypes.income)(incomeCategories),
+  ...allWithType(categoriesTypes.expense)(expenseCategories),
 ];
