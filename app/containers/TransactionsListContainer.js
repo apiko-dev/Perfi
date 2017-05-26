e44fd7f9c7e2aba8afac9f662c5fa9391e3ad2ee
danilichev <v.danilichev@gmail.com>
2017-05-26 17:28:09 +0300

    Added draft for filtering by date

--- app/containers/TransactionsListContainer.js
+++ app/containers/TransactionsListContainer.js
@@ -2,19 +2,25 @@ import { connect } from 'react-redux';
 import R from 'ramda';
 import { TransactionsList } from '../components';
 
-const filterByAccount = (accountId, transactions) =>
-  R.filter(R.propEq('account', accountId), R.values(transactions.byId));
+const filterByAccountAndDate = (transactions, accountId, period) => {
+  const isRightAccount = R.propEq('account', accountId);
+  const isRightDate = () => true;
+  const filterFn = period ? R.both(isRightAccount, isRightDate) : isRightAccount;
+
+  return R.filter(filterFn, R.values(transactions.byId));
+};
+
 
 const groupByCategories = R.groupBy(R.prop('category'));
 
-const handleTransactions = R.pipe(filterByAccount, groupByCategories);
+const handleTransactions = R.pipe(filterByAccountAndDate, groupByCategories);
 
 const pickCategories = (transactionsMap, categories) =>
   R.pick(R.keys(transactionsMap), categories.byId);
 
-const mapStateToProps = ({ accounts, categories, transactions }, { account }) => {
+const mapStateToProps = ({ accounts, categories, transactions }, { account, period }) => {
   const currentAccount = account || accounts.byId[0];
-  const transactionsByCategories = handleTransactions(currentAccount.id, transactions);
+  const transactionsByCategories = handleTransactions(transactions, currentAccount.id, period);
 
   return {
     categories: pickCategories(transactionsByCategories, categories),
