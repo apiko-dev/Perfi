e368fefa32e13af2ad2c493f95f30a38d6a9f8fe
danilichev <v.danilichev@gmail.com>
2017-06-01 22:02:39 +0300

    Moved NavigatorContainer to navigator folder

--- app/containers/index.js
+++
@@ -1,5 +0,0 @@
-import NavigatorContainer from './NavigatorContainer';
-
-export {
-  NavigatorContainer,
-};
--- app/navigation/NavigatorContainer.js
+++ app/navigation/NavigatorContainer.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import Navigator from '../navigation';
+import Navigator from './NavigatorView';
 
 const NavigatorContainer = connect(({ navigator, storage }) => ({
   navigator,
