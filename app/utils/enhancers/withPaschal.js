cf76a4c568f2843df08749a42cce03689436f268
Oleksandr Yuriev <yurolex7@gmail.com>
2018-08-22 10:31:32 +0300

    Update withPaschal.js

--- app/utils/enhancers/withPaschal.js
+++ app/utils/enhancers/withPaschal.js
@@ -3,8 +3,6 @@ import { Alert } from 'react-native';
 const bang = (val) => {
   const text = {
     2014: 'Apiko company was found in 2014 😱',
-    1.111: 'The designer of this project was Vitaliy Kobytsia 😜',
-    16.0004: 'Alex Yuriev began work on this project on April 16, 2018 😄',
   }[val];
 
   if (text) Alert.alert(`${val}!`, text, [{ text: '💋' }]);
